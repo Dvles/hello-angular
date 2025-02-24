3530decf8c60a5263f279faa55b7c963014adabd
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-24 17:09:53 +0100

    created shelterLocation component

---
+++ src/app/shelter-location/shelter-location.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-location',
+  imports: [],
+  template: `
+    <p>
+      shelter-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class ShelterLocationComponent {
+
+}
