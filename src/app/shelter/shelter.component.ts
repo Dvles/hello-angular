a33800d7b72e507a436b0ea9dd719d48c57fc3a7
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-24 17:14:44 +0100

    Integrate ShelterLocationComponent into ShelterComponent
    - Imported `ShelterLocationComponent` into `shelter.component.ts`.
    - Added `ShelterLocationComponent` to the `imports` array in `@Component` metadata.
    - Updated `shelter.component.ts` template to include `<app-shelter-location>` for rendering.

--- src/app/shelter/shelter.component.ts
+++ src/app/shelter/shelter.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { ShelterLocationComponent } from '../shelter-location/shelter-location.component';
 
 @Component({
   selector: 'app-shelter',
-  imports: [],
+  imports: [CommonModule, ShelterLocationComponent],
   template: `
-    <section>      <form>        <input type="text" placeholder="Filter by region" />        <button class="primary" type="button">Search</button>      </form>    </section>
+    <section>      
+      <form>       
+         <input type="text" placeholder="Filter by region" />        
+         <button class="primary" type="button">Search</button>      
+      </form>    
+    </section>
+    <section class="results">      <app-shelter-location></app-shelter-location>    </section>
   `,
     styleUrls: ['./shelter.component.css'],
 })
