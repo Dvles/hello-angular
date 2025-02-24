c9ec4d1a7bfda7f10e15a41fc911e5d11986a8cb
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-24 16:59:13 +0100

    Create HomeComponent and update AppComponent to display Shelter layout
    - Generated a new HomeComponent using Angular CLI.
    - Imported HomeComponent into AppComponent and added it to the template.
    - Updated the app title to 'shelters' and changed content to display "home works!" from the HomeComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
+import {ShelterComponent} from './shelter/shelter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ShelterComponent],
   template: `
-    <h1>Hello World</h1>
+        <main>      <header class="brand-name">        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" style="height:50px; width:auto;" />     </header>      <section class="content">        <app-shelter></app-shelter>      </section>    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
