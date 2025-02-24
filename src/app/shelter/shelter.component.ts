3af70f5a43271bc067ae574ed961ca41ac735160
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-24 16:49:40 +0100

    Created Shelter Component

---
+++ src/app/shelter/shelter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter',
+  imports: [],
+  template: `
+    <p>
+      shelter works!
+    </p>
+  `,
+  styles: ``
+})
+export class ShelterComponent {
+
+}
