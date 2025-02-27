535f912d750743744028f1dfb58a85d77fb4c3d3
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-27 11:10:55 +0100

    HTTP:  Refactor ShelterService to fetch data from JSON server API
    - Replaced hardcoded shelter data with API calls to fetch shelter locations.
    - Updated `getAllShelterLocations` and `getShelterLocationById` methods to use `fetch` for asynchronous data retrieval from `http://localhost:3000/locations`.
    - Changed the `submitApplication` method to maintain logging functionality while removing unnecessary `ContactMethod` enum dependency

--- src/app/shelter.service.ts
+++ src/app/shelter.service.ts
@@ -1,87 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ShelterLocation } from './shelter-location';
-import { ContactMethod } from './models/contact-method.enum';
+//import { ContactMethod } from './models/contact-method.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShelterService {
 
-    protected shelterLocationList: ShelterLocation[] = 
-    [
-      {
-        id: 1,
-        name: "Centre d'Accueil Saint-Pierre",
-        region: "Brussels-City",
-        telephone: "02/219.36.50",
-        contact_methods: [ContactMethod.Email, ContactMethod.Phone],
-        photo: "https://centredaccueil.fr/wp-content/uploads/2018/12/saintpierre.jpg",
-        adress: "Rue de la Providence, 16 — 1210 BRUXELLES",
-        wifi: true,
-        laundry: true,
-        breakfast: true
-      },
-      {
-        id: 2,
-        name: "Le Samusocial",
-        region: "Brussels-City",
-        telephone: "02/512.34.55",
-        contact_methods: [ContactMethod.Phone, ContactMethod.InPerson],
-        photo: "https://samusocial.be/wp-content/uploads/2019/02/DSC_3426-HeaderMODO-640x320.png",
-        adress: "Rue des Quatre Vents, 7 — 1000 BRUXELLES",
-        wifi: true,
-        laundry: true,
-        breakfast: true
-      },
-      {
-        id: 3,
-        name: "Centre d'Accueil d'Urgence ARIANE",
-        region: "Forest",
-        telephone: "02/346.66.60",
-        contact_methods: [ContactMethod.Email, ContactMethod.Phone],
-        photo: "https://maisondariane.ca/wp-content/uploads/2023/05/Design-sans-titre-3.png",
-        adress: "Avenue du Pont de Luttre, 132 — 1190 FOREST",
-        wifi: true,
-        laundry: false,
-        breakfast: false
-      },
-      {
-        id: 4,
-        name: "La Maison de l'Accueil",
-        region: "Molenbeek",
-        telephone: "02/420.23.89",
-        contact_methods: [ContactMethod.Email, ContactMethod.InPerson],
-        photo: "https://monument.heritage.brussels/medias/500/buildings/10801259/10801259_0062_P01.jpg",
-        adress: "Avenue de la Libération, 56 — 1080 MOLEENBEEK",
-        wifi: false,
-        laundry: false,
-        breakfast: true
-      },
-      {
-        id: 5,
-        name: "Refuge Solidarité",
-        region: "Saint-Josse",
-        telephone: "02/219.65.47",
-        contact_methods: [ContactMethod.Phone, ContactMethod.Email],
-        photo: "https://sjtn.brussels/sites/default/files/styles/l480_h320/public/news/maison-accueil-sociale.jpg?itok=a3uyPhCf",
-        adress: "Rue de l'Alliance, 23 — 1210 SAINT-JOSSE",
-        wifi: true,
-        laundry: true,
-        breakfast: false
-      }
-    ]
+    url = 'http://localhost:3000/locations';
+
+    async getAllShelterLocations(): Promise<ShelterLocation[]> {    
+      const data = await fetch(this.url);    
+      return (await data.json()) ?? [];  
+    }  
+    
+    async getShelterLocationById(id: number): Promise<ShelterLocation | undefined> {    
+      const data = await fetch(`${this.url}/${id}`);    
+      return (await data.json()) ?? {};  
+    }  
+    
+    submitApplication(firstName: string, lastName: string, email: string) {    // tslint:disable-next-line   
+    console.log(firstName, lastName, email);  }
 
   
 
-  getAllShelterLocations(): ShelterLocation[] {
-    return this.shelterLocationList;
-  }
-  getShelterLocationById(id: number): ShelterLocation | undefined {
-    return this.shelterLocationList.find((shelterLocation) => shelterLocation.id === id);
-  }
-
-  submitApplication(firstName: string, lastName: string, email: string){
-    console.log(`Shelter application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,);
-
-  }
 }
