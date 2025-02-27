50335b7e1f80aa11f1e02db102cd52e57525f185
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-27 10:42:16 +0100

    SEARCH: Initialize filteredLocationList in constructor to display all shelter locations by default
    - Modified constructor to set `filteredLocationList` to all shelter locations initially.
    - Ensures that the list of shelter locations is available for filtering when the component is loaded.

--- src/app/shelter/shelter.component.ts
+++ src/app/shelter/shelter.component.ts
@@ -32,5 +32,6 @@ export class ShelterComponent {
 
   constructor(){
     this.shelterLocationList = this.shelterService.getAllShelterLocations();
+    this.filteredLocationList = this.shelterLocationList;
   }
 }
