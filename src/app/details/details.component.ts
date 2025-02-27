a88d7c00b797b0c617d8c5eaeb2ff8044bb98d58
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-27 09:39:37 +0100

    FORM: Created DetailsComponent to display detailed information about a specific shelter location
    - Injects ActivatedRoute to retrieve the shelter ID from the URL.
    - Injects ShelterService to fetch shelter details based on the ID.
    - Displays shelter name, address, contact methods, and amenities.
    Includes a form for users to apply as volunteers, utilizing Angular's ReactiveFormsModule.
    - Lays the foundation for handling volunteer applications in the future.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import { ShelterService} from '../shelter.service';
+import { ShelterLocation } from '../shelter-location';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, ShelterService],
   template: `
-    <p>
-      details works!
-    </p>
+    <article>
+      <img
+        class="listing-photo"
+        [src]="shelterLocation?.photo"
+        alt="{ shelterLocation?.name }}"
+        crossorigin
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ shelterLocation?.name }}</h2>
+        <p class="listing-location">{{ shelterLocation?.adress }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this shelter location</h2>
+        <ul>
+          <li>Contact methods: {{ shelterLocation?.contact_methods }}</li>
+          <li>Does this location have wifi: {{ shelterLocation?.wifi }}</li>
+          <li>Does this location have laundry: {{ shelterLocation?.laundry }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to volonteer here </h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
+    </article>
   `,
   styles: ``
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(ShelterService);
+  housingLocation: ShelterLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 }
